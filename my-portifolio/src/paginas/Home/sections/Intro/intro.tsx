2211e5f567b927e6298a0bf35784d1fb92d29f96
FernandesThalita <thalitahfernades@gmail.com>
2025-02-02 14:10:50 -0300

    config theme e primeira section (Intro, introducao)

--- my-portifolio/src/paginas/Home/sections/Intro/intro.tsx
+++ my-portifolio/src/paginas/Home/sections/Intro/intro.tsx
@@ -3,12 +3,14 @@ import Grid from '@mui/material/Grid';
 // import img_Avatar from "../../../../assets/img/thalita.jpeg";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import theme from "../../../../theme";
 
 const Intro = () => {
 
     const IntroStyle = styled("div")(() => ({
-        backgroundColor: "#e4e3e3",
+        backgroundColor: theme.palette.primary.main,
         height: "100vh",
+        color: theme.palette.secondary.dark
     }))
     // const ImgStyle = styled("img")(() => ({
     //     width: "30%",
@@ -29,7 +31,7 @@ const Intro = () => {
                         </Grid> */}
                         <Grid item xs={12} md={12}>
                             <Typography textAlign="center" variant="h1">Thalita de Lima</Typography>
-                            <Typography variant="h2" textAlign="center">Desenvolvimento de Sistemas</Typography>
+                            <Typography  variant="h2" textAlign="center">Desenvolvimento de Sistemas</Typography>
 
                             <Grid container display="flex" justifyContent="center">
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
--- my-portifolio/src/theme.ts
+++ my-portifolio/src/theme.ts
@@ -3,10 +3,11 @@ import { createTheme, responsiveFontSizes } from '@mui/material';
 let theme = createTheme({
   palette: {
     primary: {
-      main: "#d05ebc",
+      main: "#e4e3e3",
     },
     secondary: {
       main: "#d05ebc",
+      dark: "#111"
     },
   },
   typography: {
